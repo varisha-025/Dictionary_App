aa38d18dcaea453abe47a43c0bcab6cc96acdfe5
Varisha Rashid <varisharashid@Varishas-MacBook-Air.local>
2021-12-06 22:16:28 +0530

    improved design and new feature added

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ let notFound = document.querySelector(".not_found");
 let defBox = document.querySelector(".def");
 let audioBox = document.querySelector(".audioBox");
 let suggestion = document.querySelector(".didYouMean");
-
-
+let wordofday = document.getElementById("word")
+let suggest_box = document.getElementById("suggest_box")
+let date =document.getElementById("date")
 search.addEventListener("click", function clicked(e) {
     e.preventDefault();
+    document.getElementById("our").style.display="block";
     audioBox.innerHTML = '';
     notFound.innerHTML = '';
     suggestion.innerHTML = '';
@@ -28,18 +30,20 @@ search.addEventListener("click", function clicked(e) {
 });
 
 function suggest(data) {
-    load_div.style.display = "none";
-    suggest_h1 = document.createElement('h3');
-    suggest_h1.innerText = "DID YOU MEAN ?";
-    suggestion.appendChild(suggest_h1)
+    load_div.style.display= "none";
+    document.getElementById("our").style.display="none";
+    document.getElementById("day").style.display="none";
+    
+    document.getElementById("suggest_h3").style.display="block";
+    // suggestion.appendChild(suggest_h1)
 
-    let html = `<ul>`;
+    let html = `<div class="d-flex flex-column bd-highlight mb-3">`;
     for (key in data) {
-        html += `<li>${data[key]}</li>`
+        html += `<div class="p-2 bd-highlight">${data[key]}</div>`
     }
-    html += `</ul>`;
-    console.log(html)
-    defBox.innerHTML = html;
+    html+=`</div>`
+    // console.log(html)
+    suggest_box.innerHTML = html;
 }
 
 function display(defList) {
@@ -66,7 +70,7 @@ async function getData(word) {
     // if results are found
 
     load_div.style.display = "none";
-    console.log(data[0].shortdef[0])
+    // console.log(data[0].shortdef[0])
     display(data[0].shortdef[0])
 
     // Render sound in the DOM
@@ -85,3 +89,38 @@ function renderSound(soundName) {
     aud.controls = true;
     audioBox.appendChild(aud);
 }
+
+async function getRandomWord(){
+    try{
+    const response = await fetch(`https://random-word-api.herokuapp.com/word?number=1`);
+    const data = await response.json();
+    console.log(data)
+    return data
+}
+    catch(err){
+        console.log("this is the error",err)
+    }
+    
+}
+
+
+wordofday.addEventListener("click",async ()=>{
+    let got=await getRandomWord();
+    // document.getElementsByClassName("div2").style.display="block";
+    let f=got[0]
+    // var d = new Date();
+    // var curr_date = d.getDate();
+    // var curr_month = d.getMonth();
+    // curr_month++;
+    // var curr_year = d.getFullYear();
+    // let dt=curr_date + "-" + curr_month + "-" + curr_year
+    // day.innerText= dt;
+    document.getElementById("display").innerText=f;
+})
+
+
+
+// for button
+
+
+  
